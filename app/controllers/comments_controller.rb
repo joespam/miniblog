899243a5ce7addb1d1dedc41a7d16da04e795d69
Shehab Nabil <freudfloyd@gmail.com>
2015-11-08 18:18:24 +0100

    " issue 2, fixed."

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,17 +4,33 @@ class CommentsController < ApplicationController
 		@post_id = params[:post_id]
 	end
 	def create
-		comment = user_params.dup
+		comment = comment_params.dup
 		comment[:post_id] = params[:post_id]
 		comment[:user_id] = session[:user_id]
-		Comment.create comment
-		Comment.save
+		new_comment = Comment.create comment
+		new_comment.save
 		redirect_to post_path params[:post_id]
 #		redirect_to user_path session[:user_id]
 	end
+	def edit
+		@comment = Comment.find params[:id]
+
+	end
+
+	def update
+		comment = Comment.find params[:id]
+		post = comment.post
+		if comment and comment.update_attributes comment_params.dup
+			flash[:notice] = "Comment updated successfully"
+			redirect_to post_path post.id
+		else
+			flash[:alert] = "Updating failed."
+			redirect_to edit_post_comment_path post.id, comment.id
+		end			
+	end
 
 	private
-	def user_params
+	def comment_params
 		params.require(:comment).permit(:body, :user_id, :post_id)
 	end	
 end
--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,12 +1,17 @@
 module CommentsHelper
 	def comment_author_fullname_link comment_id
 		comment = Comment.find comment_id
-		pp comment
-		puts comment_id
-		pp comment.user
-		puts "----------------------------------------"
+		
 		user = comment.user
 		return link_to "#{user.fname} #{user.lname}", user_path(user.id)
+	end
+
+	def can_comment comment_id
+		comment = Comment.find comment_id
+		user = comment.user
+		post = comment.post
+		return session[:user_id] == user.id ||
+				session[:user_id] == post.user_id
 	end
 
 end
